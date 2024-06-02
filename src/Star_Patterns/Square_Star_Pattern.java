0b3a34601f8a4d5eeec5aca44a6165660f516e64
SohelShaikh_2712 <sohelshaikh2799@gmail.com>
2024-06-02 14:48:30 +0530

    java program to print the square star pattern

---
+++ src/Star_Patterns/Square_Star_Pattern.java
@@ -0,0 +1,15 @@
+package Star_Patterns;
+
+public class Square_Star_Pattern {
+    public static void main(String[]args){
+
+        for(int row = 1; row <= 5; row++){
+            for(int col = 1; col <=5; col++)
+                    System.out.print("* ");
+
+
+            System.out.println(" ");
+
+        }
+    }
+}
