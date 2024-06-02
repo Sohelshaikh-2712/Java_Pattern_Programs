6b87e715c09a8e7edef180168178eb2bf3ef6c50
SohelShaikh_2712 <sohelshaikh2799@gmail.com>
2024-06-02 14:49:16 +0530

    java program to print the triangle pattern

---
+++ src/Star_Patterns/Star_Pattern_One.java
@@ -0,0 +1,12 @@
+package Star_Patterns;
+
+public class Star_Pattern_One {
+    public static void main(String [] args){
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
